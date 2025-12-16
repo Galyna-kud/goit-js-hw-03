bcdd1ca318ad19ab0d04b30b253b5ead374e13c1
Galyna Kud <galynakud121@gmail.com>
2025-12-16 21:18:05 +0200

    file homework

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+function slugify(title) {
+    const lowerTitle = title.toLowerCase();
+    const slug = lowerTitle.split(" ").join("-");
+    return slug;
+    
+}
+
+console.log(slugify("Arrays for beginners")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
